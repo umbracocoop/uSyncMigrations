e9a458de29add0acfc20cd69c3ac8d3618c9a55e
Rune Grønkjær <rg@teasolutions.dk>
2023-06-16 11:19:40 +0200

    Coop baseline migrators

---
+++ uSync.Migrations/Migrators/Coop/BaselineChainFormPickerMigrator.cs
@@ -0,0 +1,11 @@
+﻿using uSync.Migrations.Context;
+using uSync.Migrations.Migrators.Models;
+
+namespace uSync.Migrations.Migrators;
+
+[SyncMigrator( "Baseline.ChainFormPicker" )]
+public class BaselineChainFormPickerMigrator : SyncPropertyMigratorBase
+{
+    public override string GetEditorAlias(SyncMigrationDataTypeProperty propertyModel, SyncMigrationContext context)
+        => "Baseline.ChainFormPicker";
+}
---
+++ uSync.Migrations/Migrators/Coop/BaselineContentRelationsMigrator.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using uSync.Migrations.Context;
+using uSync.Migrations.Migrators.Models;
+
+namespace uSync.Migrations.Migrators;
+
+[SyncMigrator( "baseline.content-relations.controller" )]
+public class BaselineContentRelationsMigrator : SyncPropertyMigratorBase
+{
+    public override string GetEditorAlias(SyncMigrationDataTypeProperty propertyModel, SyncMigrationContext context)
+        => "Umbraco.Label";
+
+  public override object? GetConfigValues( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context ) {
+    var config = new JObject {
+      { "umbracoDataValueType", "STRING" }
+    };
+    return config;
+  }
+
+  public override string? GetContentValue( SyncMigrationContentProperty contentProperty, SyncMigrationContext context ) {
+    return "SKAL SLETTES SENERE";
+  }
+}
