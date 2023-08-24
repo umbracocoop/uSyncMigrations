cd1315c8aadbd48522cafc52da4433b31ef52dfc
Camilla Skibsted <cs@teasolutions.dk>
2023-08-24 09:18:49 +0200

    RTE: Delete ref to deleted media

--- uSync.Migrations/Migrators/Core/RichTextBoxMigrator.cs
+++ uSync.Migrations/Migrators/Core/RichTextBoxMigrator.cs
@@ -1,9 +1,15 @@
 ﻿using Newtonsoft.Json.Linq;
+using SixLabors.ImageSharp;
+using System;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Extensions;
 using uSync.Migrations.Context;
 using uSync.Migrations.Extensions;
 using uSync.Migrations.Migrators.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace uSync.Migrations.Migrators;
 
@@ -57,10 +63,42 @@ public class RichTextBoxMigrator : SyncPropertyMigratorBase {
   }
 
   public override string? GetContentValue( SyncMigrationContentProperty contentProperty, SyncMigrationContext context ) {
-    var richTextValue = string.Empty;
 
-    if ( string.IsNullOrWhiteSpace( contentProperty.Value ) == false )
+    var richTextValue = string.Empty;
+    if ( string.IsNullOrWhiteSpace( contentProperty.Value ) == false ) {
       richTextValue = GuidExtensions.LocalLink2Udi( contentProperty.Value );
+    }
+
+    string pattern = @"<img.*data-udi=""(umb://media.*)"".*>";
+
+    Match m = Regex.Match( richTextValue, pattern, RegexOptions.IgnoreCase );
+
+    if ( m.Success ) {
+
+      //Get umb://media.*
+      Group g = m.Groups[1];
+
+      if ( g.Success && !string.IsNullOrEmpty( g.ToString() ) ) {
+        List<string> splitOn = g.ToString().Split( "/" ).ToList();
+
+        string? mediaUda = splitOn.LastOrDefault();
+
+        if ( Guid.TryParse( mediaUda, out Guid guid ) == false && UdiParser.TryParse<GuidUdi>( mediaUda, out var udi ) == true ) {
+          guid = udi.Guid;
+        }
+
+        string doesGuidExist = context.Content.GetAliasByKey( guid );
+
+        if ( string.IsNullOrEmpty( doesGuidExist ) ) {
+          string replacedStr = Regex.Replace( richTextValue, pattern, "" );
+
+          richTextValue = replacedStr;
+        }
+
+      }
+
+
+    }
 
     return richTextValue;
   }
