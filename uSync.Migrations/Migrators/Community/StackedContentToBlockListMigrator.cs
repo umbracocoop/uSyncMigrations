ed90de4e46361ae07f8b8b55f412974afd7a8726
Camilla Skibsted <camilla.skibsted@twoday.com>
2023-12-04 14:04:10 +0100

    Fix to NC'er

--- uSync.Migrations/Migrators/Community/StackedContentToBlockListMigrator.cs
+++ uSync.Migrations/Migrators/Community/StackedContentToBlockListMigrator.cs
@@ -11,151 +11,154 @@ using uSync.Migrations.Migrators.Models;
 
 namespace uSync.Migrations.Migrators;
 
-[SyncMigrator("Our.Umbraco.StackedContent")]
+[SyncMigrator( "Our.Umbraco.StackedContent" )]
 public class StackedContentToBlockListMigrator : SyncPropertyMigratorBase
 {
-    Lazy<SyncPropertyMigratorCollection> _migrators;
+  Lazy<SyncPropertyMigratorCollection> _migrators;
 
-    public StackedContentToBlockListMigrator(Lazy<SyncPropertyMigratorCollection> migrators)
+  public StackedContentToBlockListMigrator( Lazy<SyncPropertyMigratorCollection> migrators )
+  {
+    _migrators = migrators;
+  }
+
+  public override string GetEditorAlias( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context )
+      => UmbConstants.PropertyEditors.Aliases.BlockList;
+
+  public override object? GetConfigValues( SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context )
+  {
+    var contentTypes = dataTypeProperty.PreValues?.GetPreValueOrDefault( "contentTypes", "[]" ) ?? "[]";
+    var maxItems = dataTypeProperty.PreValues?.GetPreValueOrDefault( "maxItems", 0 ) ?? 0;
+    var singleItemMode = dataTypeProperty.PreValues?.GetPreValueOrDefault( "singleItemMode", 0 ) ?? 0;
+
+    var blocks = JsonConvert
+        .DeserializeObject<List<StackedContentConfigurationBlock>>( contentTypes )?
+        .Select( x => new BlockListConfiguration.BlockConfiguration
+        {
+          ContentElementTypeKey = x.ContentTypeKey,
+          Label = x.NameTemplate,
+        } )
+        .ToArray();
+
+    if (blocks?.Any() == true)
     {
-        _migrators = migrators;
+      foreach (var elementTypeKey in blocks.Select( x => x.ContentElementTypeKey ))
+      {
+        context.ContentTypes.AddElementType( elementTypeKey );
+      }
     }
 
-    public override string GetEditorAlias(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
-        => UmbConstants.PropertyEditors.Aliases.BlockList;
+    var validationLimit = singleItemMode == 1
+         ? new BlockListConfiguration.NumberRange { Min = 1, Max = 1 }
+         : new BlockListConfiguration.NumberRange { Min = 0, Max = maxItems };
 
-    public override object? GetConfigValues(SyncMigrationDataTypeProperty dataTypeProperty, SyncMigrationContext context)
+    return new BlockListConfiguration
     {
-        var contentTypes = dataTypeProperty.PreValues?.GetPreValueOrDefault("contentTypes", "[]") ?? "[]";
-        var maxItems = dataTypeProperty.PreValues?.GetPreValueOrDefault("maxItems", 0) ?? 0;
-        var singleItemMode = dataTypeProperty.PreValues?.GetPreValueOrDefault("singleItemMode", 0) ?? 0;
+      Blocks = blocks ?? Array.Empty<BlockListConfiguration.BlockConfiguration>(),
+      ValidationLimit = validationLimit,
+    };
+  }
 
-        var blocks = JsonConvert
-            .DeserializeObject<List<StackedContentConfigurationBlock>>(contentTypes)?
-            .Select(x => new BlockListConfiguration.BlockConfiguration
-            {
-                ContentElementTypeKey = x.ContentTypeKey,
-                Label = x.NameTemplate,
-            })
-            .ToArray();
+  public override string? GetContentValue( SyncMigrationContentProperty contentProperty, SyncMigrationContext context )
+  {
 
-        if (blocks?.Any() == true)
-        {
-            foreach (var elementTypeKey in blocks.Select(x => x.ContentElementTypeKey))
-            {
-                context.ContentTypes.AddElementType(elementTypeKey);
-            }
-        }
+    return string.Empty;
 
-        var validationLimit = singleItemMode == 1
-             ? new BlockListConfiguration.NumberRange { Min = 1, Max = 1 }
-             : new BlockListConfiguration.NumberRange { Min = 0, Max = maxItems };
-
-        return new BlockListConfiguration
-        {
-            Blocks = blocks ?? Array.Empty<BlockListConfiguration.BlockConfiguration>(),
-            ValidationLimit = validationLimit,
-        };
+    if (string.IsNullOrWhiteSpace( contentProperty.Value ))
+    {
+      return string.Empty;
     }
 
-    public override string? GetContentValue(SyncMigrationContentProperty contentProperty, SyncMigrationContext context)
+    var items = JsonConvert.DeserializeObject<IList<StackedContentItem>>( contentProperty.Value );
+    if (items?.Any() != true)
     {
-        if (string.IsNullOrWhiteSpace(contentProperty.Value))
+      return string.Empty;
+    }
+
+    var contentData = new List<BlockItemData>();
+
+    var layout = new List<BlockListLayoutItem>();
+
+    foreach (var item in items)
+    {
+      var contentTypeAlias = context.ContentTypes.GetAliasByKey( item.ContentTypeKey );
+
+      foreach (var (propertyAlias, value) in item.Values)
+      {
+        var editorAlias = context.ContentTypes.GetEditorAliasByTypeAndProperty( contentTypeAlias, propertyAlias );
+
+        if (editorAlias == null)
         {
-            return string.Empty;
+          continue;
         }
 
-        var items = JsonConvert.DeserializeObject<IList<StackedContentItem>>(contentProperty.Value);
-        if (items?.Any() != true)
+        var migrator = _migrators.Value
+            .FirstOrDefault( x => x.Editors.InvariantContains( editorAlias.OriginalEditorAlias ) );
+
+        if (migrator == null)
         {
-            return string.Empty;
+          continue;
         }
 
-        var contentData = new List<BlockItemData>();
+        var childProperty = new SyncMigrationContentProperty( editorAlias.OriginalEditorAlias,
+            contentTypeAlias, propertyAlias,
+            value?.ToString() ?? string.Empty );
 
-        var layout = new List<BlockListLayoutItem>();
+        item.Values[propertyAlias] = migrator.GetContentValue( childProperty, context );
+      }
 
-        foreach (var item in items)
-        {
-            var contentTypeAlias = context.ContentTypes.GetAliasByKey(item.ContentTypeKey);
+      var block = new BlockItemData
+      {
+        ContentTypeKey = item.ContentTypeKey,
+        Udi = Udi.Create( UmbConstants.UdiEntityType.Element, item.Key ),
+        RawPropertyValues = item.Values,
+      };
 
-            foreach (var (propertyAlias, value) in item.Values)
-            {
-                var editorAlias = context.ContentTypes.GetEditorAliasByTypeAndProperty(contentTypeAlias, propertyAlias);
+      layout.Add( new BlockListLayoutItem { ContentUdi = block.Udi } );
 
-                if (editorAlias == null)
-                {
-                    continue;
-                }
+      contentData.Add( block );
+    }
 
-                var migrator = _migrators.Value
-                    .FirstOrDefault(x => x.Editors.InvariantContains(editorAlias.OriginalEditorAlias));
+    if (contentData.Any() == false)
+    {
+      return string.Empty;
+    }
 
-                if (migrator == null)
-                {
-                    continue;
-                }
-
-                var childProperty = new SyncMigrationContentProperty(editorAlias.OriginalEditorAlias,
-                    contentTypeAlias, propertyAlias,
-                    value?.ToString() ?? string.Empty);
-
-                item.Values[propertyAlias] = migrator.GetContentValue(childProperty, context);
-            }
-
-            var block = new BlockItemData
-            {
-                ContentTypeKey = item.ContentTypeKey,
-                Udi = Udi.Create(UmbConstants.UdiEntityType.Element, item.Key),
-                RawPropertyValues = item.Values,
-            };
-
-            layout.Add(new BlockListLayoutItem { ContentUdi = block.Udi });
-
-            contentData.Add(block);
-        }
-
-        if (contentData.Any() == false)
-        {
-            return string.Empty;
-        }
-
-        var model = new BlockValue
-        {
-            ContentData = contentData,
-            Layout = new Dictionary<string, JToken>
+    var model = new BlockValue
+    {
+      ContentData = contentData,
+      Layout = new Dictionary<string, JToken>
             {
                 { UmbConstants.PropertyEditors.Aliases.BlockList, JArray.FromObject(layout) },
             },
-        };
+    };
 
-        return JsonConvert.SerializeObject(model, Formatting.Indented);
-    }
+    return JsonConvert.SerializeObject( model, Formatting.Indented );
+  }
 
-    internal class StackedContentItem
-    {
-        [JsonProperty("icContentTypeGuid")]
-        public Guid ContentTypeKey { get; set; }
+  internal class StackedContentItem
+  {
+    [JsonProperty( "icContentTypeGuid" )]
+    public Guid ContentTypeKey { get; set; }
 
-        [JsonProperty("key")]
-        public Guid Key { get; set; }
+    [JsonProperty( "key" )]
+    public Guid Key { get; set; }
 
-        [JsonProperty("name")]
-        public string? Name { get; set; }
+    [JsonProperty( "name" )]
+    public string? Name { get; set; }
 
-        [JsonProperty("icon")]
-        public string? Icon { get; set; }
+    [JsonProperty( "icon" )]
+    public string? Icon { get; set; }
 
-        [JsonExtensionData]
-        public Dictionary<string, object?> Values { get; set; } = null!;
-    }
+    [JsonExtensionData]
+    public Dictionary<string, object?> Values { get; set; } = null!;
+  }
 
-    internal class StackedContentConfigurationBlock
-    {
-        [JsonProperty("icContentTypeGuid")]
-        public Guid ContentTypeKey { get; set; }
+  internal class StackedContentConfigurationBlock
+  {
+    [JsonProperty( "icContentTypeGuid" )]
+    public Guid ContentTypeKey { get; set; }
 
-        [JsonProperty("nameTemplate")]
-        public string? NameTemplate { get; set; }
-    }
+    [JsonProperty( "nameTemplate" )]
+    public string? NameTemplate { get; set; }
+  }
 }
